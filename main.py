5887bd1f3fa75a519e90aa767a0b71f8e2d9bdbf
namialus <sulaiman.atayi@hotmail.com>
2024-05-20 21:54:27 +0200

    v1.2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import yt_dlp
 # URL of the YouTube playlist
 
-def download_playlist_specific(playlist_url:string,start:int):
+def download_playlist_specific(playlist_url:str)->list:
 
 
     # Options for downloading the playlist
@@ -9,9 +9,18 @@ def download_playlist_specific(playlist_url:string,start:int):
         'format': 'best',
         'outtmpl': r'C:\Users\Schueler\Desktop\private\series\qhave_talkh_2\%(playlist_index)s_%(title)s.%(ext)s',  # Output template
         'yes-playlist': True,
-        'playlist_items': '47-' # Indicates that the URL points to a playlist
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         # Download the playlist
-        ydl.extract_info(playlist_url, download=True)
+        playlist_dwurl = ydl.extract_info(playlist_url, download=False)
+        
+        return playlist_dwurl
+
+
+arrayoflist = download_playlist_specific('https://www.youtube.com/watch?v=viukm2i-eYY&list=PLwXXfk5wsyAB0jtiodt1i6NB5rvv_ZiKd')
+
+print(arrayoflist['entries'][0]['url'])
+
+for index,info in enumerate(arrayoflist):
+    print(info[18])
