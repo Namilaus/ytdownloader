71c996f9b12e092c594666b07b9fde9d91962db4
namialus <sulaiman.atayi@hotmail.com>
2024-06-27 22:02:22 +0200

    gitignore updated

---
+++ databaseSql.py
@@ -0,0 +1,124 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+class database:
+
+    def __init__(
+        self, hostname: str, username: str, password: str, database: str
+    ) -> None:
+
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.database = database
+
+    def connection_db(self):
+        try:
+            # Connect to the MySQL server
+            connection = mysql.connector.connect(
+                host=self.hostname,
+                user=self.username,
+                password=self.password,
+                database=self.database,
+                port=13497,
+            )
+
+            # print(connection)
+
+        except mysql.connector.Error as error:
+            # Print an error message if connection fails
+            print("Connection failed:", error)
+            return
+        return connection
+
+    def insert_into_playlist(self, yt_url: str, data: dict) -> int:
+        try:
+            connection = self.connection_db()
+            # for element in range(0,data['playlist_length']):
+            # print(data['titles'][element],data['urls'][element])
+            mycommand = connection.cursor()
+            mycommand.execute(
+                "INSERT INTO playlist(yt_url, length) VALUES(%s,%s)",
+                (yt_url, data["playlist_length"]),
+            )
+            mycommand.execute("SELECT id FROM playlist ORDER BY id DESC LIMIT 1")
+            playlistID = mycommand.fetchone()  # get playlistId for the sql realiton
+
+            for element in range(
+                0, data["playlist_length"]
+            ):  # inserting all of the playlist video into video sql table
+                mycommand.execute(
+                    "INSERT INTO video (title, yt_dw) VALUES (%s, %s)",
+                    (data["titles"][element], data["urls"][element]),
+                )
+                mycommand.execute("SELECT id FROM video ORDER BY id DESC LIMIT 1")
+                videoID = mycommand.fetchone()
+                mycommand.execute(
+                    "INSERT INTO videotoplaylist (videoID,playlistID) VALUES (%s, %s)",
+                    (videoID[0], playlistID[0]),
+                )
+        except Error as error:
+
+            print(error)
+            return 405
+
+        finally:
+
+            connection.commit()
+            connection.close()
+            return 200
+
+    def insert_video(self, yt_url: str, data) -> int:
+        try:
+            connection = self.connection_db()
+            mycommand = connection.cursor()
+            mycommand.execute(
+                "INSERT INTO video(title, yt_url, yt_dw) VALUES(%s, %s, %s)",
+                (data["titles"], yt_url, data["urls"]),
+            )
+        except Error as error:
+            print(error)
+            return 505
+        finally:
+            connection.commit()
+            connection.close()
+            return 200
+
+    def serach_for_video(self, yt_url: str) -> list:
+        try:
+            connection = self.connection_db()
+
+            with connection.cursor() as mycommand:
+                mycommand.execute("SELECT * FROM video WHERE yt_url = %s", (yt_url,))
+                data = mycommand.fetchall()
+                if data is None:
+                    return None
+
+            connection.close()
+            return data
+
+        except Error as error:
+            print(error)
+
+    def serach_for_playlist(self, yt_url: str) -> list:
+        try:
+            connection = self.connection_db()
+
+            with connection.cursor() as mycommand:  # instead of JOINs using WHERE because of speed and performance
+                mycommand.execute(
+                    "SELECT * FROM video WHERE id IN ( SELECT videoID FROM videotoplaylist WHERE playlistID IN (SELECT id FROM playlist WHERE yt_url = %s ))",
+                    (yt_url,),
+                )
+                data = mycommand.fetchall()
+                if data is None:
+                    return None
+
+            connection.close()
+            return data
+
+        except Error as error:
+            print(error)
+
+
+# print(serach_for_playlist("https://www.youtube.com/watch?v=viukm2i-eYY&list=PLwXXfk5wsyAB0jtiodt1i6NB5rvv_ZiKd"))
---
+++ frontend/main.js
@@ -0,0 +1,161 @@
+// Elements
+const container = document.getElementsByClassName("container")[0]
+const input = document.getElementById("url")
+const button = document.getElementById("download_button")
+const option = document.getElementById("option")
+
+function showLoading() {
+    document.getElementById('loading-animation').style.display = 'flex';
+}
+
+function hideLoading() {
+    document.getElementById('loading-animation').style.display = 'none';
+}
+
+function errorMsg(text){
+    const errorMsg = document.createElement("p")
+    errorMsg.classList.add("error")
+    errorMsg.textContent = text
+    errorMsg.style.display = "block"
+    container.appendChild(errorMsg)
+}
+
+function removeErrorMsg(){
+    errorMsg = document.getElementsByClassName("error")[0]
+    if(errorMsg !== undefined){
+        container.removeChild(errorMsg)
+    }
+}
+
+function removeLinkFromContainer(){
+    
+        if(document.getElementsByClassName("result")[0]){
+        const results = document.getElementsByClassName("result")
+        for(let index = 0; index < results.length; index++){
+            results[index].style.display = "none"
+        }
+    }
+
+    return
+}
+
+function addLinktoContainer(url,title){
+    const result = document.createElement("div")
+    const resultUrl = document.createElement("p")
+    const resultTitle = document.createElement("p")
+    const span = document.createElement("span")
+    const download_link = document.createElement("a")
+
+    result.classList.add("result")
+    resultTitle.textContent =  "Title: " + title
+    resultUrl.textContent = "Download Link: "
+    download_link.textContent = "Click Here"
+    download_link.setAttribute("href",url)
+    download_link.setAttribute("target","_blank")
+
+    resultTitle.appendChild(span)
+    result.appendChild(resultTitle)
+    resultUrl.appendChild(download_link)
+    result.appendChild(resultUrl)
+    container.appendChild(result)
+
+    result.style.display = "block"
+}
+
+
+button.addEventListener('click',(e)=>{
+    
+    removeErrorMsg()
+    showLoading();
+    e.preventDefault()
+   
+    if(input.value=== '' || option.value === ''){
+        hideLoading()
+        errorMsg("Either input is blank or option is not selected you may put the url or select one of them options please!")
+        console.log(option.value)
+        return
+    }
+    else if(input.value!=='' && option.value === 'download_video'){
+    fetch('https://ytdownloader.atayi.net:8443/download_video',{
+        method:'POST',
+        mode:"cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body:JSON.stringify({url:input.value})
+    })
+        .then(response => { 
+            
+            if(response.status !== 200){
+                removeErrorMsg()
+                hideLoading()
+                errorMsg("Something went wrong try again, Please make sure that you chose the right Option and type the right link in input")
+                return
+            }
+            return response.json()} )
+        .then((response)=>{
+            removeLinkFromContainer()
+            hideLoading()
+            addLinktoContainer(response.url,response.title)
+    })
+    
+}
+    else if(input.value!=='' && option.value === 'download_playlist'){
+        fetch('https://ytdownloader.atayi.net:8443/download_playlist',{
+            method:'POST',
+            mode:"cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify({url:input.value})
+            })
+            .then(response => { 
+                if(response.status !== 200){
+                    removeErrorMsg()
+                    hideLoading()
+                    errorMsg("Something went wrong try again, Please make sure that you chose the right Option and type the right link in input")
+                    
+                    return
+                }
+                return response.json() } )
+            .then((response)=>{
+                removeLinkFromContainer()
+                hideLoading()
+                const lengthOfPlaylist = response.titles.length
+                for(let i = 0; i < lengthOfPlaylist; i++){
+                    addLinktoContainer(response.urls[i],response.titles[i])
+                }
+            })
+    }
+    else if(input.value!=='' && option.value === 'download_playlist_from'){
+        const startIndex  = parseInt(document.getElementById("startIndex").value)
+        console.log(startIndex)
+        fetch('https://ytdownloader.atayi.net:8443/download_playlist_specific',{
+            method:'POST',
+            mode:"cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify({url:input.value,startIndex:startIndex})
+            })
+            .then(response => { 
+                if(response.status !== 200){
+                    removeErrorMsg()
+                    hideLoading()
+                    errorMsg("Something went wrong try again, Please make sure that you chose the right Option and type the right link in input")
+                    
+                    return
+                }
+                return response.json()} )
+            .then((response)=>{
+                removeLinkFromContainer()
+                hideLoading()
+                const lengthOfPlaylist = response.titles.length
+                for(let i = 0; i < lengthOfPlaylist; i++){
+                    addLinktoContainer(response.urls[i],response.titles[i])
+                }
+            })
+    }
+})
+
+
